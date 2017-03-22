73811efbf056c8fad302aa0501706af3a8474d1a
Fabrice Daugan <fabrice.daugan@gfi.fr>
2017-03-22 16:01:08 +0100

    Coverage API test, waiting for ligoj/bootstrap#4

--- plugin-api-test/src/test/java/org/ligoj/app/TestAbstractJpaTest.java
+++ plugin-api-test/src/test/java/org/ligoj/app/TestAbstractJpaTest.java
@@ -2,11 +2,13 @@ package org.ligoj.app;
 
 import javax.persistence.EntityManager;
 
+import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mockito;
-
 import org.ligoj.app.iam.IamConfiguration;
 import org.ligoj.app.iam.IamProvider;
+import org.ligoj.bootstrap.core.SpringUtils;
+import org.mockito.Mockito;
+import org.springframework.context.ApplicationContext;
 
 /**
  * Test class of {@link AbstractJpaTest}
@@ -27,4 +29,34 @@ public class TestAbstractJpaTest extends AbstractJpaTest {
 		getGroup();
 		persistSystemEntities();
 	}
+
+	/**
+	 * Restore original Spring application context.<br>
+	 * TODO Remove this with LB 1.6.1, see ligoj/bootstrap#4
+	 */
+	@Test
+	public void testRestoreAppalicationContextNull() {
+		ApplicationContext context = SpringUtils.getApplicationContext();
+		applicationContext = null;
+		restoreAppalicationContext();
+		Assert.assertSame(context, SpringUtils.getApplicationContext());
+	}
+
+	/**
+	 * Restore original Spring application context.<br>
+	 * TODO Remove this with LB 1.6.1, see ligoj/bootstrap#4
+	 */
+	@Test
+	public void testRestoreAppalicationContextNotNull() {
+		final ApplicationContext context = SpringUtils.getApplicationContext();
+		try {
+			applicationContext = Mockito.mock(ApplicationContext.class);
+			restoreAppalicationContext();
+			Assert.assertSame(applicationContext, SpringUtils.getApplicationContext());
+		} finally {
+			// Really restore
+			SpringUtils.setSharedApplicationContext(context);
+		}
+	}
+
 }
