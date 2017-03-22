48f46fd4c60f3644b7eef71324e3c9a3405d2341
Fabrice Daugan <fabrice.daugan@gfi.fr>
2017-03-22 19:02:38 +0100

    Fix coverage, linked to ligoj/bootstrap#5

--- plugin-api/src/test/java/org/ligoj/app/api/NormalizerTest.java
+++ plugin-api/src/test/java/org/ligoj/app/api/NormalizerTest.java
@@ -1,5 +1,8 @@
 package org.ligoj.app.api;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -34,6 +37,23 @@ public class NormalizerTest {
 	public void normalize() {
 		Assert.assertEquals("c", Normalizer.normalize(" C "));
 		Assert.assertEquals("c", Normalizer.normalize("c"));
+	}
+
+	@Test
+	public void testCoverage()
+			throws SecurityException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+		coverageSingleton(Normalizer.class);
+	}
+
+	/**
+	 * TODO Removes whe utility checker ligoj/bootstrap#5 is ready
+	 */
+	private <S> void coverageSingleton(Class<S> singletonClass)
+			throws SecurityException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+		final Constructor<S> constructor = singletonClass.getDeclaredConstructor();
+		Assert.assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+		constructor.setAccessible(true);
+		constructor.newInstance();
 	}
 
 }
