0161c4c357631a3f798391c3bc544a9fac0d2dfa
Fabrice Daugan <fabrice.daugan@gfi.fr>
2017-03-23 23:23:20 +0100

    Add authentication contributor extension

---
+++ plugin-api/src/main/java/org/ligoj/app/api/IAuthenticationContributor.java
@@ -0,0 +1,22 @@
+package org.ligoj.app.api;
+
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import org.springframework.security.core.Authentication;
+
+/**
+ * Login contributor. he unique function is called just after a successful authentication.
+ */
+@FunctionalInterface
+public interface IAuthenticationContributor {
+
+	/**
+	 * Call to contribute to the final response after the authentication.
+	 * 
+	 * @param response
+	 *            The current {@link ResponseBuilder}. May have already been visited.
+	 * @param authentication
+	 *            The current authentication. Should be while building the response.
+	 */
+	void accept(ResponseBuilder response, Authentication authentication);
+}
