40469e0a90160f13714c8cbf6d4f9056eb4c2a61
Fabrice Daugan <fabrice.daugan@gmail.com>
2017-04-07 11:44:35 +0200

    Return the first matching subscription instead of expecting only one

--- plugin-api-test/src/test/java/org/ligoj/app/TestAbstractAppTest.java
+++ plugin-api-test/src/test/java/org/ligoj/app/TestAbstractAppTest.java
@@ -66,6 +66,7 @@ public class TestAbstractAppTest extends AbstractAppTest {
 		em = Mockito.mock(EntityManager.class);
 		final TypedQuery<Object> typeQuery = Mockito.mock(TypedQuery.class);
 		Mockito.when(typeQuery.setParameter(ArgumentMatchers.anyInt(), ArgumentMatchers.any())).thenReturn(typeQuery);
+		Mockito.when(typeQuery.setMaxResults(1)).thenReturn(typeQuery);
 		Mockito.when(typeQuery.getSingleResult()).thenReturn(3);
 		Mockito.when(em.createQuery(ArgumentMatchers.anyString(), ArgumentMatchers.any())).thenReturn(typeQuery);
 		Assert.assertEquals(3, getSubscription("some", "service"));
