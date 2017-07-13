0cb121ec21ba4b74418437fd942516c7a69eac9e
Fabrice Daugan <fabrice.daugan@gfi.fr>
2017-07-13 15:01:46 +0200

    Lint SonarQube

--- plugin-core/src/main/java/org/ligoj/app/dao/SecuritySpringDataListener.java
+++ plugin-core/src/main/java/org/ligoj/app/dao/SecuritySpringDataListener.java
@@ -31,14 +31,14 @@ public class SecuritySpringDataListener implements AfterJpaBeforeSpringDataListe
 	private static final String IS_TEAM_LEADER = "$project.team_leader=$user OR ";
 	private static final String VISIBLE_PROJECT = IS_TEAM_LEADER + VISIBLE_GROUP;
 
-	private static final String MEMBER_GROUP = "   $exists (SELECT cg.description AS dn, cg.id FROM $cm AS cm LEFT JOIN $cg AS cg ON (cg.id=cm.$q(group)) WHERE cm.$q(user)=$user) AS s_cg6 WHERE s_cg6.id=$arg OR $exists $cg WHERE s_cg6.dn LIKE CONCAT('%,',description) $end $end";
-	private static final String MEMBER_GROUP2 = "  $exists $cm AS cm LEFT JOIN $cg AS cg ON (cg.id=cm.$q(group)) WHERE cm.$q(user)=$user AND cg.id=$arg OR $exists $cg AS cg WHERE cg.description LIKE CONCAT('%,',description) $end $end";
-	private static final String MEMBER_COMPANY = " $exists (SELECT cc.description AS dn, cc.id FROM $cu AS cu LEFT JOIN $cc AS cc ON (cc.id=cu.company)   WHERE cu.id=$user)       AS s_cc7 WHERE s_cc7.id=$arg OR $exists $cc WHERE s_cc7.dn LIKE CONCAT('%,',description) $end $end";
-	private static final String MEMBER_COMPANY2 = "$exists $cu AS cu LEFT JOIN $cc AS cc ON (cc.id=cu.company)   WHERE cu.id=$user       AND cc.id=$arg OR $exists $cc AS cc WHERE cc.description LIKE CONCAT('%,',description) $end $end";
-	private static final String MEMBER_PKEY = "    $exists (SELECT pj.pkey FROM $cm AS cm LEFT JOIN $cg AS cg ON (cg.id=cm.$q(group)) LEFT JOIN $cpg AS cpg ON (cg.id=cpg.$q(group)) LEFT JOIN $pj AS pj ON (pj.id=cpg.project) WHERE cm.$q(user)=$user) AS s_pj8 WHERE s_pj8.pkey=$arg $end";
-	private static final String MEMBER_PKEY2 = "   $exists $cm AS cm LEFT JOIN $cg AS cg ON (cg.id=cm.$q(group)) LEFT JOIN $cpg AS cpg ON (cg.id=cpg.$q(group)) LEFT JOIN $pj AS pj ON (pj.id=cpg.project) WHERE cm.$q(user)=$user AND pj.pkey=$arg $end";
-	private static final String MEMBER_PROJECT = " $exists (SELECT cpg.project AS id FROM $cm AS cm LEFT JOIN $cg AS cg ON (cg.id=cm.$q(group)) LEFT JOIN $cpg AS cpg ON (cg.id=cpg.$q(group)) WHERE cm.$q(user)=$user) AS s_pj9 WHERE s_pj9.id=$arg $end";
-	private static final String MEMBER_PROJECT2 = "$exists $cm AS cm LEFT JOIN $cg AS cg ON (cg.id=cm.$q(group)) LEFT JOIN $cpg AS cpg ON (cg.id=cpg.$q(group)) WHERE cm.$q(user)=$user AND cpg.project=$arg $end";
+	private static final String IN_GROUP = "   $exists (SELECT cg.description AS dn, cg.id FROM $cm AS cm LEFT JOIN $cg AS cg ON (cg.id=cm.$q(group)) WHERE cm.$q(user)=$user) AS s_cg6 WHERE s_cg6.id=$arg OR $exists $cg WHERE id=$arg AND s_cg6.dn LIKE CONCAT('%,',description) $end $end";
+	private static final String IN_COMPANY = " $exists (SELECT cc.description AS dn, cc.id FROM $cu AS cu LEFT JOIN $cc AS cc ON (cc.id=cu.company)   WHERE cu.id=$user)       AS s_cc7 WHERE s_cc7.id=$arg OR $exists $cc WHERE id=$arg AND s_cc7.dn LIKE CONCAT('%,',description) $end $end";
+	private static final String IN_GROUP2 = "  $exists $cm AS cm LEFT JOIN $cg AS cg ON (cg.id=cm.$q(group)) WHERE cm.$q(user)=$user AND cg.id=$arg OR $exists $cg AS cg WHERE id=$arg AND cg.description LIKE CONCAT('%,',description) $end $end";
+	private static final String IN_COMPANY2 = "$exists $cu AS cu LEFT JOIN $cc AS cc ON (cc.id=cu.company)   WHERE cu.id=$user       AND cc.id=$arg OR $exists $cc AS cc WHERE id=$arg AND cc.description LIKE CONCAT('%,',description) $end $end";
+	private static final String IN_PKEY = "    $exists (SELECT pj.pkey FROM $cm AS cm LEFT JOIN $cg AS cg ON (cg.id=cm.$q(group)) LEFT JOIN $cpg AS cpg ON (cg.id=cpg.$q(group)) LEFT JOIN $pj AS pj ON (pj.id=cpg.project) WHERE cm.$q(user)=$user) AS s_pj8 WHERE s_pj8.pkey=$arg $end";
+	private static final String IN_PKEY2 = "   $exists $cm AS cm LEFT JOIN $cg AS cg ON (cg.id=cm.$q(group)) LEFT JOIN $cpg AS cpg ON (cg.id=cpg.$q(group)) LEFT JOIN $pj AS pj ON (pj.id=cpg.project) WHERE cm.$q(user)=$user AND pj.pkey=$arg $end";
+	private static final String IN_PROJECT = " $exists (SELECT cpg.project AS id FROM $cm AS cm LEFT JOIN $cg AS cg ON (cg.id=cm.$q(group)) LEFT JOIN $cpg AS cpg ON (cg.id=cpg.$q(group)) WHERE cm.$q(user)=$user) AS s_pj9 WHERE s_pj9.id=$arg $end";
+	private static final String IN_PROJECT2 = "$exists $cm AS cm LEFT JOIN $cg AS cg ON (cg.id=cm.$q(group)) LEFT JOIN $cpg AS cpg ON (cg.id=cpg.$q(group)) WHERE cm.$q(user)=$user AND cpg.project=$arg $end";
 	private static final String IS_TEAM_LEADER_ID = " $exists (SELECT id FROM $pj pj WHERE pj.team_leader=$user) AS pj10 WHERE pj10.id=$arg $end OR ";
 	private static final String IS_TEAM_LEADER_ID2 = "$exists $pj pj WHERE pj.team_leader=$user AND pj.id=$arg $end OR ";
 	private static final String IS_TEAM_LEADER_PK = " $exists (SELECT pkey FROM $pj pj WHERE pj.team_leader=$user) AS pj11 WHERE pj11.pkey=$arg $end OR ";
@@ -68,30 +68,6 @@ public class SecuritySpringDataListener implements AfterJpaBeforeSpringDataListe
 		// delegate on this group or one of its sub-groups
 		registerFunction(new DnFunction("visiblegroup", 5, 0, null, VISIBLE_GROUP, (sql, args) -> sql));
 
-		// Member of a group : member of this group or one of its sub-groups
-		registerFunction(new DnFunction("ingroup", 2, 0, MEMBER_GROUP, null, null, (sql, args) -> sql));
-
-		// Member of a group : member of this group or one of its sub-groups
-		registerFunction(new DnFunction("ingroup2", 2, 0, MEMBER_GROUP2, null, null, (sql, args) -> sql));
-
-		// Member of a company : member of this company or one of its sub-company
-		registerFunction(new DnFunction("incompany", 2, 0, MEMBER_COMPANY, null, null, (sql, args) -> sql));
-
-		// Member of a company : member of this company or one of its sub-company
-		registerFunction(new DnFunction("incompany2", 2, 0, MEMBER_COMPANY2, null, null, (sql, args) -> sql));
-
-		// Member of a project : team leader or member of any group of this project
-		registerFunction(new DnFunction("inprojectkey", 3, 0, MEMBER_PKEY, null, IS_TEAM_LEADER_PK, (sql, args) -> sql));
-
-		// Member of a project : team leader or member of any group of this project
-		registerFunction(new DnFunction("inprojectkey2", 3, 1, MEMBER_PKEY2, null, IS_TEAM_LEADER_PK2, (sql, args) -> sql));
-
-		// Member of a project : team leader or member of any group of this project
-		registerFunction(new DnFunction("inproject", 3, 0, MEMBER_PROJECT, null, IS_TEAM_LEADER_ID, (sql, args) -> sql));
-
-		// Member of a project : team leader or member of any group of this project
-		registerFunction(new DnFunction("inproject2", 2, 0, MEMBER_PROJECT2, null, IS_TEAM_LEADER_ID2, (sql, args) -> sql));
-
 		// Visible company : member of this company or one of its sub-companies
 		// or delegate on this company or one of its sub-companies
 		registerFunction(new DnFunction("visiblecompany", 5, 0, null, VISIBLE_COMPANY, (sql, args) -> sql));
@@ -103,6 +79,56 @@ public class SecuritySpringDataListener implements AfterJpaBeforeSpringDataListe
 		// Admin DN : delegate with "can_admin" flag on the related DN of one of
 		// its parent
 		registerFunction(new DnFunction("admindn", 4, 0, "can_admin", null, (sql, args) -> sql));
+
+		// Member of a group : member of this group or one of its sub-groups
+		// Accepted signatures :
+		// - ingroup(:user_id, group.id)
+		// - ingroup(:user_id, 'fixed_group')
+		// - ingroup('fixed_user',group.id, )
+		// - ingroup('fixed_user', 'fixed_group')
+		registerFunction(new DnFunction("ingroup", 2, 1, 0, IN_GROUP, null, null, (sql, args) -> sql));
+
+		// Member of a group : member of this group or one of its sub-groups
+		// Accepted signatures :
+		// - ingroup(any user id, any group id)
+		registerFunction(new DnFunction("ingroup2", 2, 1, 0, IN_GROUP2, null, null, (sql, args) -> sql));
+
+		// Member of a company : member of this company or one of its sub-company
+		// - incompany(:user_id, company.id)
+		// - incompany(:user_id, 'fixed_company')
+		// - incompany('fixed_user',company.id, )
+		// - incompany('fixed_user', 'fixed_company')
+		registerFunction(new DnFunction("incompany", 2, 1, 0, IN_COMPANY, null, null, (sql, args) -> sql));
+
+		// Member of a company : member of this company or one of its sub-company
+		// - incompany2(any user id, any company id)
+		registerFunction(new DnFunction("incompany2", 2, 1, 0, IN_COMPANY2, null, null, (sql, args) -> sql));
+
+		// Member of a project : team leader or member of any group of this project
+		// Accepted signatures :
+		// - inprojectkey(:user_id, project.pkey, :user_id, project.pkey)
+		// - inprojectkey(:user_id, 'fixed_pkey', :user_id, 'fixed_pkey')
+		// - inprojectkey('fixed_user', 'fixed_pkey', 'fixed_user', 'fixed_pkey')
+		// - inprojectkey('fixed_user', project.pkey, 'fixed_user', project.pkey)
+		registerFunction(new DnFunction("inprojectkey", 4, 1, 0, IN_PKEY, null, IS_TEAM_LEADER_PK, (sql, args) -> sql));
+
+		// Member of a project : team leader or member of any group of this project
+		// Accepted signatures :
+		// - inprojectkey2(any user id form, any pkey form, any user id form, any pkey form)
+		registerFunction(new DnFunction("inprojectkey2", 4, 1, 0, IN_PKEY2, null, IS_TEAM_LEADER_PK2, (sql, args) -> sql));
+
+		// Member of a project : team leader or member of any group of this project
+		// Accepted signatures :
+		// - inproject(:user_id, project.id, :user_id, project.id)
+		// - inproject(:user_id, 'fixed_id', :user_id, 'fixed_id')
+		// - inproject('fixed_user', 'fixed_id', 'fixed_user', 'fixed_id')
+		// - inproject('fixed_user', project.id, 'fixed_user', project.id)
+		registerFunction(new DnFunction("inproject", 4, 1, 0, IN_PROJECT, null, IS_TEAM_LEADER_ID, (sql, args) -> sql));
+
+		// Member of a project : team leader or member of any group of this project
+		// Accepted signatures :
+		// - inproject2(any user id form, any id form, any user id form, any id form)
+		registerFunction(new DnFunction("inproject2", 4, 1, 0, IN_PROJECT2, null, IS_TEAM_LEADER_ID2, (sql, args) -> sql));
 	}
 
 	private void registerFunction(final StandardSQLFunction sqlFunction) {
@@ -142,13 +168,25 @@ public class SecuritySpringDataListener implements AfterJpaBeforeSpringDataListe
 		 */
 		private DnFunction(final String name, final int nbArgs, final int dnIndex, final String query,
 				final String access, final String filter, final BiFunction<String, List<?>, String> callback) {
+			this(name, nbArgs, dnIndex, dnIndex + 1, query, access, filter, callback);
+		}
+
+		/**
+		 * Construct a standard SQL function definition with a static return type.
+		 *
+		 * @param name
+		 *            The name of the function.
+		 */
+		private DnFunction(final String name, final int nbArgs, final int dnIndex, final int userIndex,
+				final String query, final String access, final String filter,
+				final BiFunction<String, List<?>, String> callback) {
 			super(name, StandardBasicTypes.BOOLEAN);
 			this.nbArgs = nbArgs;
 			this.filter = filter;
 			this.callback = callback;
 			this.access = access;
 			this.dnIndex = dnIndex;
-			this.userIndex = dnIndex + 1;
+			this.userIndex = userIndex;
 			this.query = query;
 		}
 
--- plugin-core/src/test/java/org/ligoj/app/dao/SecuritySpringDataListenerTest.java
+++ plugin-core/src/test/java/org/ligoj/app/dao/SecuritySpringDataListenerTest.java
@@ -1,12 +1,12 @@
 package org.ligoj.app.dao;
 
-import static org.junit.Assert.assertTrue;
-
 import java.util.Arrays;
 import java.util.Collections;
 
 import javax.transaction.Transactional;
 
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +23,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @Transactional
 public class SecuritySpringDataListenerTest {
 
-	private static final String P_DN = "_dn__";
+	private static final String P_ARG = "_arg__";
 	private static final String P_USER = "_user__";
+	private static final String Q_ARG = "?dn__";
+	private static final String Q_USER = "?user__";
 	@Autowired
 	private SecuritySpringDataListener listener;
 
@@ -33,54 +35,71 @@ public class SecuritySpringDataListenerTest {
 		listener.getSqlFunctions().get("visiblegroup").render(null, Collections.emptyList(), null);
 	}
 
-	private void assertFunction(final String name, final String sql, String... args) {
-		assertTrue(listener.getSqlFunctions().get(name).render(null, Arrays.asList(args), null).contains(sql));
+	private void assertFunction(final String name, final int nbQueryParam, final String sql, String... args) {
+		final String query = listener.getSqlFunctions().get(name).render(null, Arrays.asList(args), null);
+		Assert.assertEquals(nbQueryParam, StringUtils.countMatches(query, "[?]"));
+		Assert.assertTrue(query + "-- not contains --" + sql, query.contains(sql));
 	}
 
 	@Test
 	public void visibleproject() {
-		assertFunction("visibleproject", "_p__.team_leader=_user__", "_p__.id", P_USER, P_USER, P_USER, P_USER, P_USER,
-				P_USER);
+		assertFunction("visibleproject", 3, "_p__.team_leader=_user__", "_p__.id", "g", P_USER, Q_USER, Q_USER, Q_USER,
+				Q_USER);
 	}
 
 	@Test
 	public void visiblegroup() {
-		assertFunction("visiblegroup", "WHERE _dn__=s_d5.dn", P_DN, P_DN, P_USER, P_USER, P_USER);
+		assertFunction("visiblegroup", 4, "WHERE _dn__=s_d5.dn", P_ARG, Q_USER, Q_USER, Q_USER, Q_USER);
 	}
 
 	@Test
 	public void visiblecompany() {
-		assertFunction("visiblecompany", "WHERE _dn__=s_d5.dn", P_DN, P_USER, P_USER, P_USER, P_USER);
+		assertFunction("visiblecompany", 4, "WHERE _dn__=s_d5.dn", P_ARG, Q_USER, Q_USER, Q_USER, Q_USER);
 	}
 
 	@Test
 	public void writedn() {
-		assertFunction("writedn", "WHERE _dn__=s_d5.dn", P_DN, P_USER, P_USER, P_USER);
+		assertFunction("writedn", 3, "WHERE _dn__=s_d5.dn", P_ARG, Q_USER, Q_USER, Q_USER);
 	}
 
 	@Test
 	public void admindn() {
-		assertFunction("admindn", "WHERE _dn__=s_d5.dn", P_DN, P_USER, P_USER, P_USER);
+		assertFunction("admindn", 3, "WHERE _dn__=s_d5.dn", P_ARG, Q_USER, Q_USER, Q_USER);
 	}
 
 	@Test
 	public void inproject() {
-		assertFunction("inproject", "_p__.team_leader=_user__", "_p__.id", P_USER, P_USER);
+		assertFunction("inproject", 2, "_p__.team_leader=_user__", "_p__.id", Q_USER, Q_USER);
 	}
 
 	@Test
 	public void inprojectkey() {
-		assertFunction("inprojectkey", "WHERE f_pr8.pkey=_pkey__", "_pkey__", P_USER, P_USER);
-	}
-
-	@Test
-	public void incompany() {
-		assertFunction("incompany", "WHERE f_cc7.id=_company__", "_company__", P_USER);
+		assertFunction("inprojectkey", 2, "WHERE f_pr8.pkey=_pkey__", "_pkey__", Q_USER, Q_USER);
 	}
 
 	@Test
 	public void ingroup() {
-		assertFunction("ingroup", "WHERE f_cg6.id=_group__", "_group__", P_USER);
+		assertFunction("ingroup", 1, "_p__.team_leader=_user__", P_ARG, Q_USER);
+	}
+
+	@Test
+	public void incompany() {
+		assertFunction("incompany", 1, "WHERE s_cc7.id=_company__", P_ARG, Q_USER);
+	}
+
+	@Test
+	public void inprojectkey2() {
+		assertFunction("inprojectkey2", 2, "WHERE f_pr8.pkey=_pkey__", Q_ARG, Q_ARG, P_USER);
+	}
+
+	@Test
+	public void inproject2() {
+		assertFunction("inproject2", 2, "_p__.team_leader=_user__", Q_USER, Q_ARG, P_USER);
+	}
+
+	@Test
+	public void ingroup2() {
+		assertFunction("ingroup2", 1, "_p__.team_leader=_user__", Q_ARG, P_USER);
 	}
 
 }
