483c487533f5ca19c9c1f58fef8f1e019c83940a
pcieslar-gfi <33416965+pcieslar-gfi@users.noreply.github.com>
2017-11-08 12:01:58 +0100

    Add reset password service
    New interface method

--- plugin-api/src/main/java/org/ligoj/app/iam/IPasswordGenerator.java
+++ plugin-api/src/main/java/org/ligoj/app/iam/IPasswordGenerator.java
@@ -3,14 +3,28 @@ package org.ligoj.app.iam;
 /**
  * Password generator extension point.
  */
-@FunctionalInterface
 public interface IPasswordGenerator {
 
 	/**
-	 * Set a generated password to given user. The password may be generated and sent to user and may requires additional steps to be fully available to this user.
+	 * Set a generated password to given user. The password may be generated and
+	 * sent to user and may requires additional steps to be fully available to
+	 * this user.
 	 * 
 	 * @param user
 	 *            The user identifier.
 	 */
 	void generate(String user);
+
+	/**
+	 * Set a generated password to given user. The password may be generated and
+	 * sent to user and the administrator having reset it and may requires
+	 * additional steps to be fully available to this user.
+	 * 
+	 * @param user
+	 *            The user identifier.
+	 * @param admin
+	 *            The administrator identifier.
+	 * @return The generated password.
+	 */
+	String generate(String user, String admin);
 }
