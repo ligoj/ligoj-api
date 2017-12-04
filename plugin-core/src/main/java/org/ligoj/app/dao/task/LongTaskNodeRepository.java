d20de2dcedbd05f1ce29333a6a0af58096a71270
fabdouglas <fabrice.daugan@gmail.com>
2017-12-05 00:25:35 +0100

    Add task filtered by visibility

--- plugin-core/src/main/java/org/ligoj/app/dao/task/LongTaskNodeRepository.java
+++ plugin-core/src/main/java/org/ligoj/app/dao/task/LongTaskNodeRepository.java
@@ -1,5 +1,8 @@
 package org.ligoj.app.dao.task;
 
+import java.util.List;
+
+import org.ligoj.app.dao.NodeRepository;
 import org.ligoj.app.model.AbstractLongTask;
 import org.ligoj.app.model.Node;
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +18,11 @@ public interface LongTaskNodeRepository<T extends AbstractLongTask<Node, String>
 	@Query("FROM #{#entityName} i WHERE i.locked.id = :node AND i.end IS NULL")
 	T findNotFinishedByLocked(String node);
 
+	/**
+	 * Return all visible catalogs.
+	 * @param user The current principal user.
+	 * @return The visible catalogs for the current principal user.
+	 */
+	@Query("SELECT i FROM #{#entityName} i INNER JOIN i.locked AS n WHERE " + NodeRepository.VISIBLE_NODES)
+	List<T> findAllVisible(String user);
 }
