187a82e0434ad31d24d456ad6b2bfd57c26d56aa
Paul Cieslar <paul.cieslar@gfi.fr>
2017-12-15 15:04:52 +0100

    Change generate method to return the generated password

--- plugin-api/src/main/java/org/ligoj/app/iam/IPasswordGenerator.java
+++ plugin-api/src/main/java/org/ligoj/app/iam/IPasswordGenerator.java
@@ -3,28 +3,17 @@ package org.ligoj.app.iam;
 /**
  * Password generator extension point.
  */
+@FunctionalInterface
 public interface IPasswordGenerator {
 
 	/**
 	 * Set a generated password to given user. The password may be generated and
-	 * sent to user and may requires additional steps to be fully available to
+	 * sent to user and may require additional steps to be fully available to
 	 * this user.
 	 * 
 	 * @param user
 	 *            The user identifier.
-	 */
-	void generate(String user);
-
-	/**
-	 * Set a generated password to given user. The password may be generated and
-	 * sent to user and the administrator having reset it and may requires
-	 * additional steps to be fully available to this user.
-	 * 
-	 * @param user
-	 *            The user identifier.
-	 * @param admin
-	 *            The administrator identifier.
 	 * @return The generated password.
 	 */
-	String generate(String user, String admin);
+	String generate(String user);
 }
