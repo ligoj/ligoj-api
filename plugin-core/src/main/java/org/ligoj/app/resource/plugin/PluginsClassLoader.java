e643bc88421481c2c1d13fd844168490f04b51e6
Fabrice Daugan <fabrice.daugan@gmail.com>
2018-03-23 14:04:49 +0100

    Add home based directory operations for nodes

--- plugin-core/src/main/java/org/ligoj/app/resource/plugin/PluginsClassLoader.java
+++ plugin-core/src/main/java/org/ligoj/app/resource/plugin/PluginsClassLoader.java
@@ -13,9 +13,11 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
@@ -34,22 +36,19 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Class Loader which load jars in {@value #PLUGINS_DIR} directory inside the
- * home directory.
+ * Class Loader which load jars in {@value #PLUGINS_DIR} directory inside the home directory.
  */
 @Slf4j
 @Getter
 public class PluginsClassLoader extends URLClassLoader {
 
 	/**
-	 * System property name pointing to the home directory. When undefined,
-	 * system user home directory will be used
+	 * System property name pointing to the home directory. When undefined, system user home directory will be used
 	 */
 	public static final String HOME_DIR_PROPERTY = "ligoj.home";
 
 	/**
-	 * Default home directory part used in addition of system user home
-	 * directory : "/home/my-user" for sample.
+	 * Default home directory part used in addition of system user home directory : "/home/my-user" for sample.
 	 */
 	public static final String HOME_DIR_FOLDER = ".ligoj";
 	/**
@@ -64,7 +63,8 @@ public class PluginsClassLoader extends URLClassLoader {
 	/**
 	 * Pattern used to extract the version from a JAR plugin file name.
 	 */
-	private static final Pattern VERSION_PATTERN = Pattern.compile("(-(\\d[\\da-zA-Z]*(\\.[\\da-zA-Z]+){1,3}(-SNAPSHOT)?))\\.jar$");
+	private static final Pattern VERSION_PATTERN = Pattern
+			.compile("(-(\\d[\\da-zA-Z]*(\\.[\\da-zA-Z]+){1,3}(-SNAPSHOT)?))\\.jar$");
 
 	/**
 	 * The application home directory.
@@ -83,8 +83,7 @@ public class PluginsClassLoader extends URLClassLoader {
 	protected final boolean safeMode;
 
 	/**
-	 * Initialize the plug-in {@link URLClassLoader} and the related
-	 * directories.
+	 * Initialize the plug-in {@link URLClassLoader} and the related directories.
 	 * 
 	 * @throws IOException
 	 *             exception when reading plug-ins directory
@@ -124,7 +123,8 @@ public class PluginsClassLoader extends URLClassLoader {
 
 		// Remove old plug-in from the list
 		final Map<String, String> mostRecentPlugins = new TreeMap<>(Comparator.reverseOrder());
-		versionFiles.keySet().stream().sorted(Comparator.reverseOrder()).filter(p -> !mostRecentPlugins.containsKey(versionFiles.get(p)))
+		versionFiles.keySet().stream().sorted(Comparator.reverseOrder())
+				.filter(p -> !mostRecentPlugins.containsKey(versionFiles.get(p)))
 				.forEach(p -> mostRecentPlugins.put(versionFiles.get(p), p));
 
 		// Add the filtered plug-in files to the class-path
@@ -140,8 +140,8 @@ public class PluginsClassLoader extends URLClassLoader {
 	/**
 	 * Return the plug-in class loader from the current class loader.
 	 * 
-	 * @return the closest {@link PluginsClassLoader} instance from the current
-	 *         thread's {@link ClassLoader}. May be <code>null</code>.
+	 * @return the closest {@link PluginsClassLoader} instance from the current thread's {@link ClassLoader}. May be
+	 *         <code>null</code>.
 	 */
 	public static PluginsClassLoader getInstance() {
 		return getInstance(Thread.currentThread().getContextClassLoader());
@@ -152,8 +152,8 @@ public class PluginsClassLoader extends URLClassLoader {
 	 * 
 	 * @param cl
 	 *            The {@link ClassLoader} to inspect.
-	 * @return the closest {@link PluginsClassLoader} instance from the current
-	 *         thread's {@link ClassLoader}. May be <code>null</code>.
+	 * @return the closest {@link PluginsClassLoader} instance from the current thread's {@link ClassLoader}. May be
+	 *         <code>null</code>.
 	 */
 	public static PluginsClassLoader getInstance(final ClassLoader cl) {
 		if (cl == null) {
@@ -190,8 +190,7 @@ public class PluginsClassLoader extends URLClassLoader {
 	}
 
 	/**
-	 * Copy a resource as needed to be exported from the JAR plug-in to the
-	 * home.
+	 * Copy a resource as needed to be exported from the JAR plug-in to the home.
 	 */
 	private void copyExportedResource(final String plugin, final Path targetExport, final Path root, final Path from) {
 		final Path dest = targetExport.resolve(root.relativize(from).toString());
@@ -200,7 +199,8 @@ public class PluginsClassLoader extends URLClassLoader {
 			try {
 				copy(from, dest);
 			} catch (final IOException e) {
-				throw new PluginException(plugin, String.format("Unable to copy exported resource %s to %s", from, dest.toString()), e);
+				throw new PluginException(plugin,
+						String.format("Unable to copy exported resource %s to %s", from, dest.toString()), e);
 			}
 		}
 	}
@@ -216,7 +216,8 @@ public class PluginsClassLoader extends URLClassLoader {
 		}
 	}
 
-	private void addVersionFile(final Map<String, Path> versionFileToPath, final Map<String, String> versionFiles, final Path path) {
+	private void addVersionFile(final Map<String, Path> versionFileToPath, final Map<String, String> versionFiles,
+			final Path path) {
 		final String file = path.getFileName().toString();
 		final Matcher matcher = VERSION_PATTERN.matcher(file);
 		final String noVersionFile;
@@ -240,14 +241,13 @@ public class PluginsClassLoader extends URLClassLoader {
 	}
 
 	/**
-	 * Convert a version to a comparable string and following the semver
-	 * specification. Maximum 4 version ranges are accepted.
+	 * Convert a version to a comparable string and following the semver specification. Maximum 4 version ranges are
+	 * accepted.
 	 * 
 	 * @param version
 	 *            The version string to convert. May be <code>null</code>
-	 * @return The given version to be comparable with another version. Handle
-	 *         the 'SNAPSHOT' case considered has oldest than the one without
-	 *         this suffix.
+	 * @return The given version to be comparable with another version. Handle the 'SNAPSHOT' case considered has oldest
+	 *         than the one without this suffix.
 	 * @see PluginsClassLoader#toExtendedVersion(String)
 	 */
 	public static String toExtendedVersion(final String version) {
@@ -255,13 +255,13 @@ public class PluginsClassLoader extends URLClassLoader {
 		final String[] allFragments = { "0", "0", "0", "0" };
 		final String[] versionFragments = ObjectUtils.defaultIfNull(StringUtils.split(version, "-."), allFragments);
 		System.arraycopy(versionFragments, 0, allFragments, 0, versionFragments.length);
-		Arrays.stream(allFragments).map(s -> StringUtils.leftPad(StringUtils.leftPad(s, 7, '0'), 8, 'Z')).forEach(fileWithVersionExp::append);
+		Arrays.stream(allFragments).map(s -> StringUtils.leftPad(StringUtils.leftPad(s, 7, '0'), 8, 'Z'))
+				.forEach(fileWithVersionExp::append);
 		return fileWithVersionExp.toString();
 	}
 
 	/**
-	 * Compute the right home directory for the application from the system
-	 * properties.
+	 * Compute the right home directory for the application from the system properties.
 	 * 
 	 * @return The computed home directory.
 	 */
@@ -270,7 +270,8 @@ public class PluginsClassLoader extends URLClassLoader {
 		if (System.getProperty(HOME_DIR_PROPERTY) == null) {
 			// Non standard home directory
 			homeDir = Paths.get(System.getProperty("user.home"), HOME_DIR_FOLDER);
-			log.info("Home directory is '{}', resolved from current home user location. Use '{}' system property to override this path",
+			log.info(
+					"Home directory is '{}', resolved from current home user location. Use '{}' system property to override this path",
 					homeDir, HOME_DIR_PROPERTY);
 		} else {
 			// Home directory inside the system user's home directory
@@ -281,9 +282,8 @@ public class PluginsClassLoader extends URLClassLoader {
 	}
 
 	/**
-	 * Get a file reference for a specific subscription. This file will use the
-	 * subscription as a context to isolate it, and using the related node and
-	 * the subscription's identifier. The parent directory is created as needed.
+	 * Get a file reference for a specific subscription. This file will use the subscription as a context to isolate it,
+	 * and using the related node and the subscription's identifier. The parent directories are created as needed.
 	 * 
 	 * @param subscription
 	 *            The subscription used a context of the file to create.
@@ -291,23 +291,87 @@ public class PluginsClassLoader extends URLClassLoader {
 	 *            The file fragments.
 	 * @return The file reference.
 	 * @throws IOException
-	 *             When the file creation failed.
+	 *             When the parent directories creation failed.
+	 * @deprecated Use {@link #toPath(Subscription, String...)} instead.
+	 * @since 2.2.4
+	 * @see {@link #toPath(Subscription, String...)}
 	 */
-	public File toFile(Subscription subscription, final String... fragments) throws IOException {
-		Path parent = toPath(getHomeDirectory(), subscription.getNode());
-		parent = parent.resolve(String.valueOf(subscription.getId()));
-
-		// Ensure the t
-		for (int i = 0; i < fragments.length; i++) {
-			parent = parent.resolve(fragments[i]);
-		}
-		FileUtils.forceMkdir(parent.getParent().toFile());
-		return parent.toFile();
+	public File toFile(final Subscription subscription, final String... fragments) throws IOException {
+		return toPath(subscription, fragments).toFile();
 	}
 
 	/**
-	 * Convert a {@link Node} to a {@link Path} inside the given parent
-	 * directory.
+	 * Get a file reference for a specific subscription. This file will use the subscription as a context to isolate it,
+	 * and using the related node and the subscription's identifier. The parent directories are created as needed.
+	 * 
+	 * @param subscription
+	 *            The subscription used a context of the file to create.
+	 * @param fragments
+	 *            The file fragments.
+	 * @return The {@link Path} reference.
+	 * @throws IOException
+	 *             When the parent directories creation failed.
+	 * @since 2.2.4
+	 */
+	public Path toPath(final Subscription subscription, final String... fragments) throws IOException {
+		return toPath(toPath(subscription.getNode()).resolve(String.valueOf(subscription.getId())), fragments);
+	}
+
+	/**
+	 * Get a file reference for a specific node. This file will use the node as a context to isolate it. The parent
+	 * directories are created as needed.
+	 * 
+	 * @param node
+	 *            The related node.
+	 * @return The {@link Path} reference.
+	 * @throws IOException
+	 *             When the parent directories creation failed.
+	 * @since 2.2.4
+	 */
+	public Path toPath(final Node node) throws IOException {
+		final Path file = toPath(toFragments(node));
+		FileUtils.forceMkdir(file.toFile());
+		return file;
+	}
+
+	/**
+	 * Convert a {@link Node} to a {@link Path} inside the home directory. The intermediate directories are also
+	 * created.
+	 * 
+	 * @param fragments
+	 *            The file fragments within the home directory.
+	 * @return The {@link Path} reference.
+	 * @throws IOException
+	 *             When the parent directories creation failed.
+	 * @since 2.2.4
+	 */
+	public Path toPath(final String... fragments) throws IOException {
+		return toPath(getHomeDirectory(), fragments);
+	}
+
+	/**
+	 * Get a file reference inside the given parent path. The parent directories are created as needed.
+	 * 
+	 * @param parent
+	 *            The parent path.
+	 * @param fragments
+	 *            The file fragments within the given parent.
+	 * @return The {@link Path} reference.
+	 * @throws IOException
+	 *             When the parent directories creation failed.
+	 */
+	private Path toPath(final Path parent, final String... fragments) throws IOException {
+		Path parentR = parent;
+		for (int i = 0; i < fragments.length; i++) {
+			parentR = parentR.resolve(fragments[i]);
+		}
+		// Ensure the parent path is created
+		FileUtils.forceMkdir(parentR.getParent().toFile());
+		return parentR;
+	}
+
+	/**
+	 * Convert a {@link Node} to a {@link Path} inside the given parent directory.
 	 * 
 	 * @param parent
 	 *            The parent path.
@@ -315,13 +379,30 @@ public class PluginsClassLoader extends URLClassLoader {
 	 *            The related node.
 	 * @return The computed sibling path.
 	 */
-	private Path toPath(final Path parent, final Node node) {
-		return (node.isRefining() ? toPath(parent, node.getRefined()) : parent).resolve(toFragmentId(node).replace(':', '-'));
+	private String[] toFragments(final Node node) {
+		final List<String> fragments = new ArrayList<>();
+		toFragments(node, fragments);
+		return fragments.toArray(new String[fragments.size()]);
 	}
 
 	/**
-	 * Return the last part of the node identifier, excluding the part of the
-	 * parent. Built like that :
+	 * Convert a {@link Node} to a {@link Path} inside the given parent directory.
+	 * 
+	 * @param parent
+	 *            The parent path.
+	 * @param node
+	 *            The related node.
+	 * @return The computed sibling path.
+	 */
+	private void toFragments(final Node node, List<String> fragments) {
+		if (node.isRefining()) {
+			toFragments(node.getRefined(), fragments);
+		}
+		fragments.add(toFragmentId(node).replace(':', '-'));
+	}
+
+	/**
+	 * Return the last part of the node identifier, excluding the part of the parent. Built like that :
 	 * <ul>
 	 * <li>node = 'service:id:ldap:ad1', fragment = 'ad1'</li>
 	 * <li>node = 'service:id:ldap', fragment = 'ldap'</li>
